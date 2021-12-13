9e7a292ff348c3656b79e92bd117756e66285d9e
Nightwind <81449663+NightwindDev@users.noreply.github.com>
2021-12-12 19:38:06 -0500

    Add files via upload

---
+++ nonewtermnavbar/Tweak.m
@@ -0,0 +1,14 @@
+#import <UIKit/UIKit.h>
+#import <substrate.h>
+
+void (*orig_didMoveToSuperview)(UIToolbar *self, SEL _cmd);
+
+void override_didMoveToSuperview(UIToolbar *self, SEL _cmd) {
+	self.superview.hidden = true;
+
+	orig_didMoveToSuperview(self, _cmd);
+}
+
+__attribute__((constructor)) static void initialize() {
+	MSHookMessageEx(NSClassFromString(@"UIToolbar"), @selector(didMoveToSuperview), (IMP) &override_didMoveToSuperview, (IMP *) &orig_didMoveToSuperview);
+}
